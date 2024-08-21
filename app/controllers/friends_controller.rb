1f93298496c4bfc96a8638406e8da75fbba5a1c9
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-21 23:14:03 +0600

    make assosiation between friend and user

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,7 +4,7 @@ class FriendsController < ApplicationController
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
-    
+
   end
 
   # GET /friends/1 or /friends/1.json
@@ -59,13 +59,14 @@ class FriendsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_friend
-      @friend = Friend.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def friend_params
-      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_friend
+    @friend = Friend.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def friend_params
+    params.require(:friend).permit(:user_id, :first_name, :last_name, :email, :phone, :twitter)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_18_161141) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_21_152510) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_18_161141) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
